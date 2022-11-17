0508873698fadd9e8abc827571a46537254d6cc3
Ivan Burakov <ivan.burakov.1998@mail.ru>
2022-11-17 17:27:29 +0600

    Initial commit

---
+++ room/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render
+
+from .models import Room, Message
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html', {'rooms': rooms})
+
+@login_required
+def room(request, slug):
+    try:
+        room = Room.objects.get(slug=slug)
+        messages = Message.objects.filter(room=room)[0:25]
+        return render(request, 'room/room.html', {'room': room, 'messages': messages})
+    except ObjectDoesNotExist:
+        room = Room.objects.create(name=slug, slug=slug)
+        messages = Message.objects.filter(room=room)[0:25]
+        return render(request, 'room/room.html', {'room': room, 'messages': messages})
