c6e7ae21cebcf7365f6db21ff4124a9b06b02247
Ivan Burakov <ivan.burakov.1998@mail.ru>
2022-11-20 02:51:34 +0600

    number of users in a room added

--- room/views.py
+++ room/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Sum
 from django.shortcuts import render
 from django.contrib.auth.models import User
-
 from .models import Room, Message
 
 @login_required
@@ -16,7 +16,13 @@ def room(request, slug):
     try:
         room = Room.objects.get(slug=slug)
         messages = Message.objects.filter(room=room)
-        return render(request, 'room/room.html', {'room': room, 'messages': messages,})
+        room_messages = Message.objects.filter(room=room)
+        users = []
+        for message in room_messages:
+            if message.user not in users:
+                users.append(message.user)
+        number_of_users = len(users)
+        return render(request, 'room/room.html', {'room': room, 'messages': messages, 'number_of_users':number_of_users})
     except ObjectDoesNotExist:
         room = Room.objects.create(name=slug, slug=slug)
         messages = Message.objects.filter(room=room)
